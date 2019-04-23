f7473d1eab5adda41aad94d0234770c84df6b9ce
Marat Levit <marat.levit@servian.com>
2019-04-23 17:59:20 +1000

    Initial support for EMR

--- README.md
+++ README.md
@@ -18,17 +18,25 @@ Open source application to programmatically clean your AWS resources based on a 
 
 To deploy this Auto Cleanup to your AWS account, follow the below steps:
 
-1. Install Serverless `npm install serverless -g`
-2. Install AWS CLI `pip3 install awscli --upgrade --user`
-3. Clone this repository `git clone https://github.com/servian/aws-auto-cleanup`
+1. Install Serverless
+   `npm install serverless -g`
+2. Install AWS CLI 
+   `pip3 install awscli --upgrade --user`
+3. Clone this repository 
+   `git clone https://github.com/servian/aws-auto-cleanup`
 4. Configure AWS CLI following the instruction at [Quickly Configuring the AWS CLI](https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-configure.html#cli-quick-configuration). Ensure the user you're configuring has the appropriate IAM permissions to create Lambda Functions, S3 Buckets, IAM Roles, and CloudFormation Stacks. It is best for administrators to deploy Auto Cleanup.
 5. If you've configure the AWS CLI using a profile, open the `serverless.yml` file and modify the `provider > profile` attribute to match your profile name.
 6. Change the custom `company` attribute within the `serverless.yml` file to your company name in order to prevent S3 Bucket name collision
-7. Change into the Auto Cleanup directory `cd aws-auto-cleanup`
-8. Install Serverless plugin `serverless plugin install -n serverless-python-requirements`
-9. Deploy Auto Cleanup `serverless deploy`
-10. Invoke Auto Cleanup for the first time `serverless invoke -f AutoCleanup`
-11. Check Auto Cleanup logs `serverless logs -f AutoCleanup`
+7. Change into the Auto Cleanup directory 
+   `cd aws-auto-cleanup`
+8. Install Serverless plugin 
+   `serverless plugin install -n serverless-python-requirements`
+9. Deploy Auto Cleanup 
+   `serverless deploy`
+10. Invoke Auto Cleanup for the first time 
+   `serverless invoke -f AutoCleanup`
+11. Check Auto Cleanup logs 
+   `serverless logs -f AutoCleanup`
 
 ### Removal
 
@@ -36,8 +44,10 @@ Auto Cleanup is deployed using the Serverless Framework which under the hood cre
 
 To remove Auto Cleanup from your AWS account, follow the below steps:
 
-1. Change into the Auto Cleanup directory `cd aws-auto-cleanup`
-2. Remove Auto Cleanup `serverless remove`
+1. Change into the Auto Cleanup directory 
+   `cd aws-auto-cleanup`
+2. Remove Auto Cleanup 
+   `serverless remove`
 
 ### Configuration
 
---
+++ auto-cleanup/emr_cleanup.py
@@ -0,0 +1,72 @@
+import boto3
+import sys
+
+from lambda_helper import *
+
+
+class EMRCleanup:
+    def __init__(self, logging, whitelist, settings, resource_tree, region):
+        self.logging = logging
+        self.whitelist = whitelist
+        self.settings = settings
+        self.resource_tree = resource_tree
+        self.region = region
+        
+        try:
+            self.client = boto3.client('emr', region_name=region)
+        except:
+            self.logging.error(str(sys.exc_info()))
+    
+    
+    def run(self):
+        self.clusters()
+        
+    
+    def clusters(self):
+        """
+        Deletes EMR Clusters.
+        """
+
+        clean = self.settings.get('services').get('emr', {}).get('clusters', {}).get('clean', False)
+        if clean:
+            try:
+                resources = self.client.list_clusters().get('Clusters')
+            except:
+                self.logging.error(str(sys.exc_info()))
+                return None
+            
+            ttl_days = self.settings.get('services').get('emr', {}).get('clusters', {}).get('ttl', 7)
+            
+            for resource in resources:
+                resource_id = resource.get('Id')
+                resource_date = resource.get('Status').get('Timeline').get('CreationDateTime')
+                resource_status = resource.get('Status').get('State')
+
+                if resource_id not in self.whitelist.get('emr', {}).get('clusters', []):
+                    delta = LambdaHelper.get_day_delta(resource_date)
+                
+                    if delta.days > ttl_days:
+                        if resource_status == 'RUNNING':
+                            if not self.settings.get('general', {}).get('dry_run', True):
+                                try:
+                                    self.client.terminate_job_flows(
+                                        JobFlowIds=[resource_id])
+                                except:
+                                    self.logging.error("Could not delete EMR Cluster '%s'." % resource_id)
+                                    self.logging.error(str(sys.exc_info()))
+                                    break
+
+                            self.logging.info("EMR Cluster '%s' was created %d days ago and has been deleted." % (resource_id, delta.days))
+                        else:
+                            self.logging.debug("EMR Cluster '%s' in state '%s' cannot be deleted." % (resource_id, resource_status))
+                    else:
+                        self.logging.debug("EMR Cluster '%s' was created %d days ago (less than TTL setting) and has not been deleted." % (resource_id, delta.days))
+                else:
+                    self.logging.debug("EMR Cluster '%s' has been whitelisted and has not been deleted." % (resource_id))
+                
+                self.resource_tree.get('AWS').setdefault(
+                    self.region, {}).setdefault(
+                        'EMR', {}).setdefault(
+                            'Clusters', []).append(resource_id)
+        else:
+            self.logging.debug("Skipping cleanup of EMR Clusters.")
