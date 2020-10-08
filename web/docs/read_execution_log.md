172151d2a43b82a3ec9ea0886777064cac54d1f3
Marat Levit <mlevit@gmail.com>
2020-10-08 14:51:40 +1100

    Added execution log API

---
+++ web/docs/read_execution_log.md
@@ -0,0 +1,61 @@
+# Read Execution Log
+
+Returns executions logs for a particular Auto Cleanup run. Each log is assigned a `{number}` at API runtime. The newest log is assigned `1`, the second newest `2` and so forth.
+
+**URL**: `/execution/{number}`
+
+**Method**: `POST`
+
+**Auth required**: No
+
+**Permissions required**: None
+
+## Success Response
+
+**Code**: `200 OK`
+
+```json
+[
+  [
+    "[string]",
+    "[string]",
+    "[string]",
+    "[string]",
+    "[string]",
+    "[string]",
+    "[string]",
+    "[string]",
+    "[string]",
+  ],
+  [...]
+]
+```
+
+**Content example**
+
+```json
+[
+  [
+    "platform",
+    "region",
+    "service",
+    "resource",
+    "resource_id",
+    "action",
+    "timestamp",
+    "dry_run_flag",
+    "execution_id"
+  ],
+  [
+    "AWS",
+    "ap-south-1",
+    "CloudFormation",
+    "Stack",
+    "AutoTag-Collector",
+    "delete",
+    "2020-10-08 02:51:50.000",
+    "true",
+    "d84c6d69-a92c-45b9-a11e-dbd01c5a8ddd"
+  ]
+]
+```
---
+++ web/read_execution_log.py
@@ -0,0 +1,95 @@
+import json
+import os
+import sys
+import time
+
+import boto3
+
+
+def get_query_results(query):
+    client = boto3.client("athena")
+
+    # execute query and retrieve query execution ID
+    try:
+        execution_id = client.start_query_execution(
+            QueryString=query,
+            ResultConfiguration={
+                "OutputLocation": f"""s3://{os.environ["ATHENARESULTSBUCKET"]}"""
+            },
+        ).get("QueryExecutionId")
+    except:
+        print(f"Could not start query '{query}'.")
+        return False
+
+    execution_status = "RUNNING"
+    execution_details = ""
+
+    # monitor query status, exit when status is not RUNNING or QUEUED
+    while execution_status in ("RUNNING", "QUEUED"):
+        try:
+            response = client.get_query_execution(QueryExecutionId=execution_id)
+
+            execution_status = response.get("QueryExecution").get("Status").get("State")
+            execution_details = (
+                response.get("QueryExecution").get("Status").get("StateChangeReason")
+            )
+
+            # sleep for 1 second so we don't spam the API
+            time.sleep(1)
+        except:
+            print(
+                f"[ERROR] Could not retrieve execution log order with error '{execution_details}'."
+            )
+            return False
+
+    # if query is successful, return results
+    if execution_status == "SUCCEEDED":
+        response = client.get_query_results(QueryExecutionId=execution_id)
+
+        # convert result into Python dictionary
+        return [
+            [data.get("VarCharValue") for data in row["Data"]]
+            for row in response["ResultSet"]["Rows"]
+        ]
+    else:
+        print(
+            f"[ERROR] Could not retrieve execution log order with error '{execution_details}'."
+        )
+        return False
+
+
+def lambda_handler(event, context):
+    client = boto3.client("athena")
+
+    # get route parameter
+    parameter = int(event.get("pathParameters").get("number"))
+
+    # get execution ID based on parameter
+    execution_id = get_query_results(
+        f"""WITH cte_max 
+                 AS (SELECT execution_id, 
+                            MAX(timestamp) AS execution_timestamp 
+                     FROM   {os.environ["AUTOCLEANUPDATABASE"]}.{os.environ["EXECUTIONLOGTABLE"]} 
+                     GROUP  BY execution_id 
+                     ORDER  BY MAX(timestamp) DESC), 
+                 cte_order 
+                 AS (SELECT ROW_NUMBER() OVER () AS execution_order, 
+                            execution_id, 
+                            execution_timestamp 
+                     FROM   cte_max) 
+            SELECT * 
+            FROM   cte_order 
+            WHERE  execution_order = {parameter}"""
+    )[1][1]
+
+    # get execution log based on execution ID
+    execution_log = get_query_results(
+        f"""SELECT * 
+            FROM   {os.environ["AUTOCLEANUPDATABASE"]}.{os.environ["EXECUTIONLOGTABLE"]}
+            WHERE  execution_id = '{execution_id}'"""
+    )
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps(execution_log),
+    }
