0ae4b85c1cd309b1e1623a9e466fe5ce8855ad01
Marat Levit <mlevit@gmail.com>
2020-10-09 16:34:08 +1100

    Simplified read execution log to replace Athena with S3 directly

--- web/api/read_execution_log.py
+++ web/api/read_execution_log.py
@@ -1,61 +1,11 @@
+import csv
+import io
 import json
 import os
 import sys
 import time
 
 import boto3
-
-
-def get_query_results(query):
-    client = boto3.client("athena")
-
-    # execute query and retrieve query execution ID
-    try:
-        execution_id = client.start_query_execution(
-            QueryString=query,
-            ResultConfiguration={
-                "OutputLocation": f"""s3://{os.environ["ATHENARESULTSBUCKET"]}"""
-            },
-        ).get("QueryExecutionId")
-    except:
-        print(f"Could not start query '{query}'.")
-        return False
-
-    execution_status = "RUNNING"
-    execution_details = ""
-
-    # monitor query status, exit when status is not RUNNING or QUEUED
-    while execution_status in ("RUNNING", "QUEUED"):
-        try:
-            response = client.get_query_execution(QueryExecutionId=execution_id)
-
-            execution_status = response.get("QueryExecution").get("Status").get("State")
-            execution_details = (
-                response.get("QueryExecution").get("Status").get("StateChangeReason")
-            )
-
-            # sleep for 1 second so we don't spam the API
-            time.sleep(1)
-        except:
-            print(
-                f"[ERROR] Could not retrieve execution log order with error '{execution_details}'."
-            )
-            return False
-
-    # if query is successful, return results
-    if execution_status == "SUCCEEDED":
-        response = client.get_query_results(QueryExecutionId=execution_id)
-
-        # convert result into Python dictionary
-        return [
-            [data.get("VarCharValue") for data in row["Data"]]
-            for row in response["ResultSet"]["Rows"]
-        ]
-    else:
-        print(
-            f"[ERROR] Could not retrieve execution log order with error '{execution_details}'."
-        )
-        return False
 
 
 def get_return(code, body):
@@ -71,34 +21,31 @@ def get_return(code, body):
 
 def lambda_handler(event, context):
     # get route parameter
-    parameter = int(event.get("pathParameters").get("number"))
+    run_number = int(event.get("pathParameters").get("number"))
 
-    if parameter in (None, ""):
-        return get_return(400, f"Execution number '{parameter}' is invalid.")
+    if run_number in (None, ""):
+        return get_return(400, f"Execution number '{run_number}' is invalid.")
 
-    # get execution ID based on parameter
-    execution_id = get_query_results(
-        f"""WITH cte_max 
-                 AS (SELECT execution_id, 
-                            MAX(timestamp) AS execution_timestamp 
-                     FROM   "{os.environ["AUTOCLEANUPDATABASE"]}".{os.environ["EXECUTIONLOGTABLE"]} 
-                     GROUP  BY execution_id 
-                     ORDER  BY MAX(timestamp) DESC), 
-                 cte_order 
-                 AS (SELECT ROW_NUMBER() OVER () AS execution_order, 
-                            execution_id, 
-                            execution_timestamp 
-                     FROM   cte_max) 
-            SELECT * 
-            FROM   cte_order 
-            WHERE  execution_order = {parameter}"""
-    )[1][1]
+    client = boto3.client("s3")
 
-    # get execution log based on execution ID
-    execution_log = get_query_results(
-        f"""SELECT * 
-            FROM   "{os.environ["AUTOCLEANUPDATABASE"]}".{os.environ["EXECUTIONLOGTABLE"]}
-            WHERE  execution_id = '{execution_id}'"""
+    response = client.list_objects_v2(
+        Bucket=os.environ["EXECUTIONLOGBUCKET"],
+    ).get("Contents")
+
+    files = []
+    for row in response:
+        files.append(row["Key"])
+
+    files.sort(reverse=True)
+
+    file_contents = (
+        client.get_object(
+            Bucket=os.environ["EXECUTIONLOGBUCKET"],
+            Key=files[run_number - 1],
+        )
+        .get("Body")
+        .read()
+        .decode("utf-8")
     )
 
-    return get_return(200, execution_log)
+    return get_return(200, list(csv.reader(file_contents.splitlines())))
