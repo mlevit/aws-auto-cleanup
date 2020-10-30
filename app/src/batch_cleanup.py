7e35329fa61b07eb53a255efa28aade83d451ca6
Marat Levit <mlevit@gmail.com>
2020-10-30 15:46:03 +1100

    Added Batch Compute Environments cleanup (WIP)

--- app/src/batch_cleanup.py
+++ app/src/batch_cleanup.py
@@ -24,8 +24,94 @@ class BatchCleanup:
         return self._client_batch
 
     def run(self):
+        self.compute_environments()
         self.job_queues()
-        self.layers()
+
+    def compute_environments(self):
+        """
+        Deletes Batch Compute Environments.
+        """
+
+        self.logging.debug("Started cleanup of Batch Compute Environments.")
+
+        clean = (
+            self.settings.get("services", {})
+            .get("batch", {})
+            .get("compute_environment", {})
+            .get("clean", False)
+        )
+        if clean:
+            try:
+                resources = self.client_batch.describe_compute_environments().get("computeEnvironments")
+            except:
+                self.logging.error("Could not list all Batch Compute Environments.")
+                self.logging.error(sys.exc_info()[1])
+                return False
+
+            ttl_days = (
+                self.settings.get("services", {})
+                .get("batch", {})
+                .get("compute_environment", {})
+                .get("ttl", 7)
+            )
+
+            for resource in resources:
+                resource_id = resource.get("computeEnvironmentName")
+                resource_date = resource.get("LastModified")
+                resource_action = "skip"
+
+                if resource_id not in self.whitelist.get("batch", {}).get(
+                    "compute_environment", []
+                ):
+                    delta = Helper.get_day_delta(resource_date)
+
+                    if delta.days > ttl_days:
+                        try:
+                            if not self._dry_run:
+                                self.client_batch.delete_compute_environment(
+                                    Compute EnvironmentName=resource_id
+                                )
+                        except:
+                            self.logging.error(
+                                f"Could not delete Batch Compute Environment '{resource_id}'."
+                            )
+                            self.logging.error(sys.exc_info()[1])
+                            resource_action = "ERROR"
+                        else:
+                            self.logging.info(
+                                f"Batch Compute Environment '{resource_id}' was last modified {delta.days} days ago "
+                                "and has been deleted."
+                            )
+                            resource_action = "DELETE"
+                    else:
+                        self.logging.debug(
+                            f"Batch Compute Environment '{resource_id}' was last modified {delta.days} days ago "
+                            "(less than TTL setting) and has not been deleted."
+                        )
+                        resource_action = "SKIP - TTL"
+                else:
+                    self.logging.debug(
+                        f"Batch Compute Environment '{resource_id}' has been whitelisted and has not been deleted."
+                    )
+                    resource_action = "SKIP - WHITELIST"
+
+                self.execution_log.get("AWS").setdefault(self.region, {}).setdefault(
+                    "Batch", {}
+                ).setdefault("Compute Environment", []).append(
+                    {
+                        "id": resource_id,
+                        "action": resource_action,
+                        "timestamp": datetime.datetime.now().strftime(
+                            "%Y-%m-%d %H:%M:%S"
+                        ),
+                    }
+                )
+
+            self.logging.debug("Finished cleanup of Batch Compute Environments.")
+            return True
+        else:
+            self.logging.info("Skipping cleanup of Batch Compute Environments.")
+            return True
 
     def job_queues(self):
         """
